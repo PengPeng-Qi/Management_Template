c99f07ed586e99035b62f4aa2cbe11816f92c13d
PengPengQ <pengpengq@outlook.com>
2023-10-16 11:13:14 +0800

    chore: add env file

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
+
+onMounted(() => {
+  document.title = import.meta.env.VITE_APP_TITLE
+})
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  server: {
+    host: true /* 默认情况下，开发服务器仅接受开发者本地连接，如果设置 host 则可以接收外部服务器连接 */,
+    open: true /* 开发服务器启动时，浏览器将自动打开浏览器进行访问 */
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
