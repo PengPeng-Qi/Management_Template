0daf44998f17387ec7e0bb8fae9373365b5ce0f6
pengpengq <1747447034@qq.com>
2023-10-16 14:15:45 +0800

    chore: add auto import apis plugin

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { RouterView } from 'vue-router'
-
 onMounted(() => {
   document.title = import.meta.env.VITE_APP_TITLE
 })
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,12 @@ export default defineConfig({
     host: true /* 默认情况下，开发服务器仅接受开发者本地连接，如果设置 host 则可以接收外部服务器连接 */,
     open: true /* 开发服务器启动时，浏览器将自动打开浏览器进行访问 */
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia']
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
