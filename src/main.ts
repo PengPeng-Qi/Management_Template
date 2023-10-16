07dd98a4403ea553b04e7bfa377ae923e431d793
PengPengQ <1747447034@qq.com>
2023-10-16 17:37:12 +0800

    feat: add element plus

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import './assets/main.css'
-
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+// reset css
+import '@/styles/reset.css'
+// element css
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
