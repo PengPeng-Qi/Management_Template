b888a75c8cfba0543114bab1842bae52d419046a
PengPengQ <pengpengq@outlook.com>
2023-10-16 19:59:54 +0800

    feat: add element icon

--- src/api/helper/checkStatus.ts
+++ src/api/helper/checkStatus.ts
@@ -1,3 +1,5 @@
+import { ElMessage } from 'element-plus'
+
 /**
  * @description: 校验网络请求状态码
  * @param {Number} status
@@ -6,36 +8,36 @@
 export const checkStatus = (status: number) => {
   switch (status) {
     case 400:
-      console.error('请求失败！请您稍后重试')
+      ElMessage.error('请求失败！请您稍后重试')
       break
     case 401:
-      console.error('登录失效！请您重新登录')
+      ElMessage.error('登录失效！请您重新登录')
       break
     case 403:
-      console.error('当前账号无权限访问！')
+      ElMessage.error('当前账号无权限访问！')
       break
     case 404:
-      console.error('你所访问的资源不存在！')
+      ElMessage.error('你所访问的资源不存在！')
       break
     case 405:
-      console.error('请求方式错误！请您稍后重试')
+      ElMessage.error('请求方式错误！请您稍后重试')
       break
     case 408:
-      console.error('请求超时！请您稍后重试')
+      ElMessage.error('请求超时！请您稍后重试')
       break
     case 500:
-      console.error('服务异常！')
+      ElMessage.error('服务异常！')
       break
     case 502:
-      console.error('网关错误！')
+      ElMessage.error('网关错误！')
       break
     case 503:
-      console.error('服务不可用！')
+      ElMessage.error('服务不可用！')
       break
     case 504:
-      console.error('网关超时！')
+      ElMessage.error('网关超时！')
       break
     default:
-      console.error('请求失败！')
+      ElMessage.error('请求失败！')
   }
 }
