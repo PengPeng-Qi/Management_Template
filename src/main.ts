a8da17fa23cea802c3cc42c0fe782c81370d14e9
pengpengq <1747447034@qq.com>
2023-12-06 16:23:23 +0800

    feat: add current mode tooltip

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ import '@/styles/element/index.scss'
 // element icon
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
+console.log(
+  `%c Current Mode: %c ${import.meta.env.MODE} `,
+  'background: #41b883; color: #fff; border-radius: 3px;',
+  'font-weight: 700;'
+)
+
 const app = createApp(App)
 // 全局注册 element icon
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
