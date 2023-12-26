b51661ea45cb31cb98e5fbb8c67bb3fb179ce3b0
pengpengq <1747447034@qq.com>
2023-12-26 17:05:30 +0800

    feat: add directives plugin

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import pinia from '@/stores'
+// global directives
+import directivesPlugin from './directives'
 // custom icons library
 import 'virtual:svg-icons-register'
 // reset css
@@ -23,4 +25,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(pinia).use(router).use(ElementPlus).mount('#app')
+app.use(pinia).use(router).use(directivesPlugin).use(ElementPlus).mount('#app')
