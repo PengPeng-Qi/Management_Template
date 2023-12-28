bf7ad2c3ae54201d5f7166a88ec51bca4bf934a1
pengpengq <1747447034@qq.com>
2023-12-28 15:51:02 +0800

    feat: on demand import element ui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import App from './App.vue'
 import router from './router'
-import ElementPlus from 'element-plus'
 import pinia from '@/stores'
 // global directives
 import directivesPlugin from './directives'
@@ -25,4 +24,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(pinia).use(router).use(directivesPlugin).use(ElementPlus).mount('#app')
+app.use(pinia).use(router).use(directivesPlugin).mount('#app')
