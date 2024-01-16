7e99ad2c63cb62649a23ce6b7bbcc988263ec048
PengPengQ <1747447034@qq.com>
2024-01-16 17:29:02 +0800

    feat: add not found view

---
+++ src/views/errors/NotFound.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <router-link to="/">Return to homepage</router-link>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  text-align: center;
+  h1 {
+    font-size: 6em;
+  }
+  p {
+    font-size: 1.5em;
+  }
+}
+</style>
