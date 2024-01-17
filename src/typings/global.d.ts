1876db15adf225c1bf6b1868f32393a52a358e5c
PengPengQ <1747447034@qq.com>
2024-01-17 15:27:14 +0800

    feat: add i18n

---
+++ src/typings/global.d.ts
@@ -0,0 +1,13 @@
+/**
+ * 拓展 vite 的 UserConfig 接口
+ */
+
+/** 拓展 ImportMetaEnv 接口 */
+declare interface ImportMetaEnv {
+  readonly VITE_ROUTER_MODE: 'hash' | 'history'
+  readonly VITE_PORT: number
+  readonly VITE_OPEN: boolean
+  readonly VITE_PUBLIC_URL: string
+  readonly VITE_Global_APP_TITLE: string
+  readonly VITE_DROP_CONSOLE: boolean
+}
