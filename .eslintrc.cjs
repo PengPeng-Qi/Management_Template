d31c81042987c56b8a5408f6c59b3e7e4f252f84
pengpengq <1747447034@qq.com>
2024-08-20 14:24:06 +0800

    refactor: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,32 @@
-/* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
   root: true,
+
   env: {
-    node: true, // 在开发过程中进行 eslint 检查
+    node: true, // 为 ESLint 提供内置的 NodeJS 的全局变量
   },
-  // 继承某些已有的规则
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier/skip-formatting",
-  ],
+
   /**
+   * 插件可以提供额外的规则、环境、解析器等
+   *
+   * 解析器：将 Vue 、Ts 等解析为 ESLint 支持的语法
+   */
+  plugins: [
+    "vue", // Vue 插件，提供了解析器解析 Vue 的 <template> <script> 等、还有规则集等
+    "@typescript-eslint", // TypeScript 插件，提供 TS 的解析器及规则集等
+    "security", // 安全插件
+  ],
+
+  // 继承某些已有的规则，需要注意顺序
+  extends: [
+    "eslint:recommended", // 配置预设，eslint 官方推荐基础规则集
+    "plugin:vue/vue3-essential", // eslint-plugin-vue 插件中 vue3 基本规则集
+    "@vue/eslint-config-typescript", // vue 官方提供的 ts 规则集
+    "@vue/eslint-config-prettier/skip-formatting", // 跳过 ESLint 的格式化
+    "plugin:security/recommended-legacy", // eslint-plugin-security 插件中的安全性基本规则集
+  ],
+
+  /**
+   * 配置规则
    * "off" 或 0    ==>  关闭规则
    * "warn" 或 1   ==>  打开的规则作为警告（不影响代码执行）
    * "error" 或 2  ==>  规则作为一个错误（代码不能执行，界面报错）
@@ -22,18 +35,15 @@ module.exports = {
     // eslint (@see: https://eslint.org/docs/latest/rules/)
     "eqeqeq": "error", // 使用严格相等符号
     "no-var": "error", // 要求使用 let 或 const 而不是 var
-    "no-multiple-empty-lines": ["error", { max: 1 }], // 不允许多个空行
 
-    // typeScript (https://typescript-eslint.io/rules)
-    // "@typescript-eslint/prefer-ts-expect-error": "error", // 禁止使用 @ts-ignore
-    // '@typescript-eslint/ban-ts-comment': 'error' // 禁止 @ts-<directive> 使用注释或要求在指令后进行描述
+    // typeScript (@see: https://typescript-eslint.io/rules)
     "@typescript-eslint/no-unused-vars": "error", // 禁止定义未使用的变量
 
-    // vue (https://eslint.vuejs.org/rules)
+    // vue (@see: https://eslint.vuejs.org/rules)
     "vue/no-mutating-props": "error", // 不允许改变组件 prop
     "vue/attribute-hyphenation": "error", // 对模板中的自定义组件强制执行属性命名样式：my-prop="prop"
-  },
-  parserOptions: {
-    ecmaVersion: "latest",
+
+    // eslint-plugin-security (@see: https://www.npmjs.com/package/eslint-plugin-security)
+    "security/detect-object-injection": "off", // 允许使用 obj[xxx] 的形式
   },
 };
