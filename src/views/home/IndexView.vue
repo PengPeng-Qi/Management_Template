14af154b6e9dd6e25207fff92a49d4eedcf4a745
pengpengq <1747447034@qq.com>
2024-08-20 15:00:51 +0800

    feat: update home

--- src/views/home/IndexView.vue
+++ src/views/home/IndexView.vue
@@ -1,12 +1,30 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
+  <div class="home">
+    <div class="home-title">Vue Template</div>
+    <div class="home-buttons">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  &-title {
+    margin-bottom: 36px;
+    font-size: xx-large;
+  }
+}
+</style>
