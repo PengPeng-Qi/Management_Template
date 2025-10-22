2c013fb4485f9a29e6a3f8daeeb05f45d8067d5a
pengpengq <1747447034@qq.com>
2025-10-22 23:02:21 +0800

    feat: add draw table

--- src/views/home/IndexView.vue
+++ src/views/home/IndexView.vue
@@ -1,30 +1,102 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const curRotateCount = ref(0);
+
+function handleRotate() {
+  const startRotation = curRotateCount.value;
+  const fullRotations = Math.floor(Math.random() * 3 + 2);
+  curRotateCount.value = startRotation + fullRotations * 12 + fullRotations;
+}
+</script>
 
 <template>
-  <div class="home">
-    <div class="home-title">Vue Template</div>
-    <div class="home-buttons">
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
+  <div class="draw-table">
+    <div>
+      <div class="draw-table__center-point">
+        <img
+          src="./assets/bg_turntable_pointer.png"
+          alt=""
+          :style="{
+            rotate: 15 + curRotateCount * 30 + 'deg',
+            transition: 'rotate 3.5s ease',
+          }"
+        />
+      </div>
+
+      <div v-for="index in 12" :key="index">
+        <img
+          src="./assets/bg_turntable_item.png"
+          alt=""
+          class="draw-table__turntable-item"
+          :style="{ rotate: 15 + index * 30 + 'deg' }"
+        />
+      </div>
+
+      <img
+        src="./assets/bg_turntable_item_active.png"
+        alt=""
+        class="draw-table__turntable-item"
+        :style="{
+          rotate: 15 + curRotateCount * 30 + 'deg',
+          transition: 'rotate 3.5s ease',
+        }"
+      />
+    </div>
+
+    <div class="draw-table__buttons">
+      <div class="draw-table__button1" @click="handleRotate">
+        <img src="./assets/btn-draw-1.png" alt="" />
+      </div>
+
+      <div class="draw-table__button2">
+        <img src="./assets/btn-draw-all.png" alt="" srcset="" />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.home {
+.draw-table {
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100vw;
-  height: 100vh;
-  &-title {
-    margin-bottom: 36px;
-    font-size: xx-large;
+  width: 644px;
+  height: 690px;
+  background-image: url("./assets/bg_act_draw_table.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  &__button1 {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 297px;
+    height: 118px;
+    margin-top: 150px;
+  }
+  &__button2 {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 297px;
+    height: 118px;
+    margin-top: 150px;
+  }
+  &__center-point {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: 2;
+    width: 272px;
+    height: 272px;
+    transform: translate(-50%, -50%);
+  }
+  &__turntable-item {
+    position: absolute;
+    top: 40px;
+    right: 250px;
+    z-index: 1;
+    transform-origin: center bottom;
   }
 }
 </style>
